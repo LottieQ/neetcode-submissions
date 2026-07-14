3fa5554c7ace7cfc9e6d6fdbc18865d8c3889513
LottieQ <117005886+LottieQ@users.noreply.github.com>
2026-07-13 19:47:28 -0700

    Add: merge-k-sorted-linked-lists - submission-2

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-2.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if ( lists== null || lists.length == 0) return null;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a , b) -> Integer.compare(a.val, b.val));
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        for(ListNode list: lists){
+            pq.offer(list);
+        }
+
+        while(!pq.isEmpty()){
+            ListNode min = pq.poll();
+            curr.next = min;
+            curr = curr.next;
+
+            if(min.next != null){
+                pq.offer(min.next);
+            }
+        }
+        return dummy.next;
+
+    }
+}
